50279c68527656616eac3c0d96e8aa2e21a6726a
aWhiteScreen <nemura.zygimantasa@gmail.com>
2026-04-24 15:59:48 +0300

    simple HTTP detection that sends you to a warning screen

--- background.js
+++ background.js
@@ -1,10 +1,30 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url) {
-    console.log("Checking URL:", tab.url);
+function isNotHTTPS(url) {
+  return url.startsWith("http://");
+}
 
-    // Placeholder for phishing detection logic
-    if (tab.url.includes("@")) {
-      console.log("Suspicious URL detected!");
-    }
+const warningPage = chrome.runtime.getURL("warning.html");
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+  // Will change to true if any phishing is detected 
+  let phishing = false;
+
+  // Only check when loading a new URL
+  if (!changeInfo.url) return;
+
+  // Checks if the URL is the extension itself or a new page, which should be ignored
+  if (changeInfo.url.startsWith("chrome-extension://") || changeInfo.url.startsWith("about:blank") || changeInfo.url.startsWith("chrome://newtab/")) return;
+  
+  if (isNotHTTPS(changeInfo.url)) {
+    console.log("Suspicious URL detected:");
+    phishing = true;
   }
-});
+
+  console.log("Checking changeinfo URL:", changeInfo.url);
+
+  // If phishing is detected, redirect to the warning page
+  if (phishing) {
+    chrome.tabs.update(tabId, { url: warningPage });
+}
+
+}); 
